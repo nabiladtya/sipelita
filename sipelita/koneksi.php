da7a4b4be0a6e5d1040d08c48c940f086afec557
nabiladtya <134028597+nabiladtya@users.noreply.github.com>
2024-09-25 18:47:29 +0700

    Delete sipelita directory

--- sipelita/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$koneksi = mysqli_connect("localhost","root","","sipelita");
-
-// cek koneksi
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-?>
